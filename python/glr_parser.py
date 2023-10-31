8ada71e2f6e362557e176adda75ef86ed2c37bf4
Thomas Müller <thomas.mueller@kiprotect.com>
2023-10-31 13:22:06 +0100

    Make GLR parser more powerful.

--- python/glr_parser.py
+++ python/glr_parser.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 import time
+import re
 
 e_grammar = [
     ['S','E','$'],#0
@@ -9,9 +10,69 @@ e_grammar = [
     ['T','n'],#4
 ]
 
+def regex(pattern):
+
+    expr = re.compile(pattern)
+
+    def match(tokens):
+        i = 0
+        match = re.match(expr, tokens)
+        if match:
+            return match[0]
+        return None
+    return match
+
+def literal(value):
+    def match(tokens):
+        i = 0
+        while i < len(tokens) and i < len(value) and tokens[i] == value[i]:
+            i += 1
+        return tokens[:i]
+    return match
+
+gospel_grammar = [
+    ['S', 'optional-whitespace', 'statements', '$'],
+    ['statements'],
+    ['statements', 'statements', 'statement', 'optional_newline'],
+    ['optional_newline'],
+    ['optional_newline', 'newline'],
+    ['newline', '\n'],
+    ['statement', 'html', 'whitespace', 'html-statement', 'optional-whitespace'],
+    ['html-statement', 'template', 'whitespace', 'template-statement'],
+    ['template-statement', 'template-tag'],
+    ['template-tag', 'tag-name', '(', 'tag-args', ')'],
+    ['tag-name', 'div'],
+    ['tag-args',],
+    ['optional-whitespace'],
+    ['optional-whitespace', 'whitespace'],
+    ['whitespace', 'whitespace', 'ws-character'],
+    ['whitespace', 'ws-character'],
+    ['ws-character', 'comment',],
+    ['ws-character', ' '],
+    ['ws-character', '\t'],
+    ['ws-character', '\n'],
+    ['comment', '#', 'anything', 'newline'],
+    ['anything', regex('[^\n]*')],
+    ['template', literal('template')],
+    ['html', literal('html')],
+    ['div', literal('div')],
+]
+
+gospel_program = [
+    'html', 'template', 'div', '(', ')', '\n', 
+]
+
+gospel_program = """#abcacbcc 2342342"§$"§$
+html # abc
+    template # aaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+        div() # bbbbbbbbbbb aaaaaaaaaaaaaa ccccc
+
+html template div()
+"""
+
 python_grammar = [
     ['S','statements','$'],
-    ['statements'],
+    ['statements'], # statements can be empty
     ['statements','statements','statement','optional_newline'],
     ['optional_newline'],
     ['optional_newline','newline'],
@@ -28,16 +89,16 @@ python_grammar = [
     ['optional_comma',],
     ['optional_comma',','],
     ['keyword_params',',','keyword_param','keyword_params'],
-    ['params','param',',','params'],
-    ['param','name'],
     ['keyword_param','name','=','expression'],
     ['expression','name'],
     ['expression','string'],
     ['string','"','characters','"'],
     ['string',"'",'characters',"'"],
     ['expression','number'],
-    ['params',],
+    ['params'],
     ['params','param'],
+    ['params','param',',','params'],
+    ['param','name'],
 ]
 
 a_grammar = [
@@ -64,7 +125,13 @@ term_grammar = [
     ['factor','s','times','factor'],
     ['factor','s'],
     ['s','number'],
-    ['s','symbol']
+    ['s','symbol'],
+    ['number', 'digits'],
+    ['digits', 'digits', 'digit'],
+    ['digits', 'digit'],
+    ['digit', '1'],
+    ['digit', '2'],
+    ['digit', '3'],
 ]
 
 e_grammar = [
@@ -77,9 +144,8 @@ class Parser(object):
 
     def __init__(self, grammar, debug=False):
         self.grammar = grammar
-        self.generate_states_and_transitions()
         self.debug = debug
-
+        self.generate_states_and_transitions()
 
     def get_rules_for_non_terminal(self, non_terminal):
         matching_rules = []
@@ -96,7 +162,8 @@ class Parser(object):
         item = self.grammar[rule][pos+1]
         if item in self.non_terminals:
             rules_for_non_terminal = self.get_rules_for_non_terminal(item)
-            print item, rules_for_non_terminal
+            if self.debug:
+                print(item, rules_for_non_terminal)
             closure = set(rules_for_non_terminal)
             rules_to_examine = rules_for_non_terminal
         while rules_to_examine:
@@ -132,11 +199,13 @@ class Parser(object):
         if rules_to_reduce:
             self.transitions[j]['__reduce__'] = rules_to_reduce
         for symbol, new_rules in rules_by_symbol.items():
-            print symbol, new_rules
+            if self.debug:
+                print(symbol, new_rules)
             new_state = set(new_rules)
             for rr in new_rules:
                 new_state = new_state | set(self.get_closure(*rr))
-            print new_state
+            if self.debug:
+                print(new_state)
             try:
                 i = self.states.index(new_state)
             except ValueError:
@@ -144,8 +213,8 @@ class Parser(object):
                 self.states.append(new_state)
                 self.extend_state(new_state)
             self.transitions[j][symbol] = i
-
-        print rules_by_symbol
+        if self.debug:
+            print(rules_by_symbol)
 
     def generate_states_and_transitions(self):
         self.non_terminals = set([rule[0] for rule in self.grammar])
@@ -157,7 +226,17 @@ class Parser(object):
         state = set(state_rules)
         self.states.append(state)
         self.extend_state(state)
-        return self.states, self.transitions
+
+        # this is an optimization to reduce the number of transitions
+        # we need to check...
+        self.callable_transitions = defaultdict(dict)
+
+        for k, v in self.transitions.items():
+            for kv, vv in v.items():
+                if callable(kv):
+                    self.callable_transitions[k][kv] = v
+
+        return self.states, self.transitions, self.callable_transitions
 
     def get_paths(self, node, depth):
         if depth == 0:
@@ -170,7 +249,7 @@ class Parser(object):
         return paths
 
     def rule_as_str(self, i):
-        return u'{} \u2192 {}'.format(self.grammar[i][0],' '.join(self.grammar[i][1:]))
+        return u'{} \u2192 {}'.format(self.grammar[i][0],' '.join([str(s) for s in self.grammar[i][1:]]))
 
     def get_stack_head(self, stack_heads, state):
         for head in stack_heads:
@@ -178,23 +257,42 @@ class Parser(object):
                 return head
         return None
 
-    def shift_stack_heads(self, stack_heads_to_process, i, input):
-        if i < len(input):
-            current_symbol = input[i]
-        else:
-            current_symbol = '$'
+    def shift_stack_heads(self, stack_heads_to_process, input):
 
-        if self.debug:
-            print "\n\nShifting stack heads with state {}".format(current_symbol)
         new_stack_heads = []
         while stack_heads_to_process:
             stack_head = stack_heads_to_process.pop()
             current_state, i, parents = stack_head
+            if self.debug:
+                print(i,"---", current_state, parents)
+            if i < len(input):
+                current_symbol = input[i]
+            else:
+                current_symbol = '$'
+
+            if self.debug:
+                print("\n\nShifting stack heads with state '{}'".format(current_symbol))
+
+            semantic_value = None
+            transition = None
+
+            for tr in self.callable_transitions[current_state]:
+                if callable(tr):
+                    tokens = tr(input[i:])
+                    if tokens:
+                        transition = self.transitions[current_state][tr]
+                        semantic_value = tokens
             if current_symbol in self.transitions[current_state]:
-                if self.debug:
-                    print "Shifting:", current_symbol
+                transition = self.transitions[current_state][current_symbol]
                 semantic_value = tuple([current_symbol])
-                new_stack_head = (self.transitions[current_state][current_symbol], i+1, [(semantic_value,stack_head)])
+
+            if self.debug:
+                print(self.transitions[current_state], current_state, transition)
+
+            if semantic_value:
+                if self.debug:
+                    print("Shifting:", semantic_value)
+                new_stack_head = (transition, i+len(semantic_value), [(semantic_value,stack_head)])
                 existing_stack_head = self.get_stack_head(new_stack_heads, new_stack_head)
                 if existing_stack_head:
                     existing_stack_head[2].append((semantic_value,stack_head))
@@ -202,23 +300,23 @@ class Parser(object):
                     new_stack_heads.append(new_stack_head)
         return new_stack_heads
 
-    def reduce_stack_heads(self, stack_heads_to_process, i, input):
+    def reduce_stack_heads(self, stack_heads_to_process, input):
         new_stack_heads = []
         while stack_heads_to_process:
             stack_head = stack_heads_to_process.pop()
+            current_state, i, parents = stack_head
             if not stack_head in new_stack_heads and i < len(input):
                 new_stack_heads.append(stack_head)
             #if self.debug:
-            #    print "\n\nProcessing stack head:",stack_head
-            current_state, i_stack, parents = stack_head
-            #print "State:", current_state, "symbol:", input[i],"i:",i
+            #    print("\n\nProcessing stack head:",stack_head)
+            #print("State:", current_state, "symbol:", input[i],"i:",i)
             #we perform all possible reduce actions...
             if '__reduce__' in self.transitions[current_state]:
                 reduce_rules = self.transitions[current_state]['__reduce__']
                 for reduce_rule in reduce_rules:
                     non_terminal = self.grammar[reduce_rule][0]
                     if self.debug:
-                        print "\nReducing with rule",self.rule_as_str(reduce_rule)
+                        print("\nReducing with rule",self.rule_as_str(reduce_rule))
                     reduce_length = len(self.grammar[reduce_rule])-1
                     paths = self.get_paths(stack_head, reduce_length)
                     for path in paths:
@@ -231,7 +329,7 @@ class Parser(object):
                         existing_stack_head = self.get_stack_head(new_stack_heads, new_state)
                         if existing_stack_head:
                             #if self.debug:
-                            #    print existing_stack_head
+                            #    print(existing_stack_head)
                             if not ancestor in [a[1] for a in existing_stack_head[2]]:
                                 existing_stack_head[2].append((semantic_value,ancestor))
                                 stack_heads_to_process.insert(0,existing_stack_head)
@@ -240,14 +338,14 @@ class Parser(object):
                                 other_semantic_value = other_ancestor[0]
                                 if other_semantic_value != semantic_value:
                                     if self.debug:
-                                        print "Competing interpretations!"
-                                        #print semantic_value,"vs.",other_semantic_value
+                                        print("Competing interpretations!")
+                                        #print(semantic_value,"vs.",other_semantic_value)
                         else:
                             new_stack_head = (new_state, i, [(semantic_value, ancestor)])
                             new_stack_heads.append(new_stack_head)
                             stack_heads_to_process.insert(0,new_stack_head)
         #if self.debug:
-        #    print new_stack_heads
+        #    print(new_stack_heads)
         return new_stack_heads
 
     def run(self, input):
@@ -256,61 +354,103 @@ class Parser(object):
         ]
         accepted_stacks = []
         output_stream = []
-        i = 0
+
         while True:
 
             if self.debug:
-                print "{} stack heads".format(len(stack_heads))
-                #print "\n".join([str(s) for s in stack_heads])
+                print("{} stack heads".format(len(stack_heads)))
+                print("\n".join([str(s) for s in stack_heads]))
 
-            new_stack_heads = self.reduce_stack_heads(stack_heads, i, input)
+            new_stack_heads = self.reduce_stack_heads(stack_heads, input)
 
-            if i == len(input)+1:
-                accepted_stacks = new_stack_heads
-                break
+            for stack_head in new_stack_heads:
+                if stack_head[1] == len(input) + 1:
+                    accepted_stacks.append(stack_head)
 
-            stack_heads = self.shift_stack_heads(new_stack_heads, i, input)
-            i+=1
+            stack_heads = self.shift_stack_heads(new_stack_heads, input)
 
             if not stack_heads:
                 break
 
         return accepted_stacks
 
+input_string = r"""
+
+model Workshop {
+
+  Usage {
+    This model represents a workshop. It ... \}
+    wer werwerjnwer werlwer lkjsdf slfjwerw elkjsddfsdcslkdjfsdfsfdj
+    sdf sfdlksjd flkjs dlfkjsdf lkjscds sdflkjsdf lkjsdf sdsdflkjsdf lkj
+    sdfölksd fsdfölk sdfölksdf ölksdcrefetpgkposdkfdgkdfgölk dscölksdf sfdsd
+    sdf sösdf ölksdf söfsdölksd fsdfölkf sdöksdf ösldkf lksdölksdölksdfsdf
+    sdföksdf sdfökdsf csdkeiwdsdfölkcd gflkdfgölkrtxcd sdfks fsdfölksdcölscd
+    sdfsdf ierimdvcsdf äölk sdfölks dfsdflksdfklscsd sdölsdfölkcsdsdc
+  }
+
+  title string {
+
+    Usage {
+        String can be used as a ....
+        sd lksdf ölsdf ölksdf ölksdfölksdfsf
+    }
+
+    Constraints {
+        
+    }
+  }
+
+  sections []WorkshopSection
+}
+
+model WorkshopSection {
+  number int
+  section Section
+}
+
+enum SectionType { PLAIN, CODE, DOCKER_COMPOSE }
+
+struct Section {
+  type SectionType
+  content blob
+}
+"""
+
 if __name__ == '__main__':
-    parser = Parser(e_grammar, debug=False)
+    parser = Parser(gospel_grammar, debug=False)
     import pprint
-    print "States:"
+    print("States:")
     pprint.pprint([list(state) for state in parser.states])
-    print "Transitions:"
+    print("Transitions:")
     pprint.pprint(dict(parser.transitions))
     for i in range(len(parser.grammar)):
-        print i,":",parser.rule_as_str(i)
+        print(i,":",parser.rule_as_str(i))
 
     input_string = ['n','+','n','+','n','$']
     input_string = ['(',')']
-    input_string = ['def','name','(','param',',','name',',','param',',','name','=','"','characters',
-                    '"', ',','keyword_param',',', ')',':','newline','indent','pass','newline','pass','dedent','newline',
-                    'name','=','number','newline','number', 'newline','funcdef']
     input_string = ['symbol','times','symbol','times','number', '+','symbol']
     input_string = ['a','a','a','a','a','a','a']
     input_string = ['b','+','b']
+    input_string = ['def','name','(','param',',','name',',','param',',','name','=','"','characters',
+                    '"', ',','keyword_param',',', ')',':','newline','indent','pass','newline','pass','dedent','newline',
+                    'name','=','number','newline','number', 'newline','funcdef']
+    input_string = gospel_program
     #input_string = ['a','n','n','a']
     #input_string = ['def','name','(',')',':','newline','indent','pass','dedent','newline']
     #input_string = ['statement','newline', 'statement','newline', 'statement','newline', 'statement','newline']
-    for i in range(18):
-        input_string = input_string+['+']+input_string[:3]
-    print(len(input_string))
+    # for i in range(18):
+    #    input_string = input_string+['+']+input_string[:3]
+    # print(len(input_string))
+    # input_string ='1+2+1+121'
     start = time.time()
-    n = 1
+    n = 100
+    print(" ".join(input_string))
     for i in range(n):
         stack_heads = parser.run(input_string)
-        print("done")
     stop = time.time()
-    print 1.0/(len(input_string)/((stop-start)/n))*1000.0
-    print "Accepted stacks: {}".format(len(stack_heads))
+    print(1.0/(len(input_string)/((stop-start)/n))*1000.0)
+    print("Accepted stacks: {}".format(len(stack_heads)))
     for stack_head in stack_heads:
         for semantic_value, parent in stack_head[2]:
-            continue
             pprint.pprint(semantic_value)
     exit(0)
